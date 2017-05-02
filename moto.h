fe599f15c23c1f658ccb52b2b1adafba32b9c396
Joao Barbirato <joaobarbirato@gmail.com>
2017-05-02 13:28:46 -0300

    Modificacoes pra gabi

--- moto.h
+++ moto.h
@@ -2,36 +2,12 @@
 #define MOTO_H
 
 using namespace std;
-
-#ifndef FILA_H
 /*
 	Cabeçalho da Fila
 */
 #include <iostream>
+#include "Fila.h"
 
-// Estrutura do nó
-struct Node{
-	char info;
-	struct Node *next;
-};
-typedef struct Node * NodePtr;
-
-// Estrutura da fila
-class Fila{
-private:
-	NodePtr primeiro;
-	NodePtr ultimo;
-	int nElementos;
-public:
-	Fila();
-	~Fila();
-	void Insere(const char &, bool &);
-	void Retira(char &, bool &);
-	void RetiraAPartirDe(const int &, bool &);
-	bool Vazia() const;
-};
-
-#endif
 
 class moto{
     public:
