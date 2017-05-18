be8712bd386daa78318e6fa5bd271e683fa78f51
gabrielissantos <gabrielisantos17.gs@gmail.com>
2017-05-17 22:01:00 -0300

    Fila.hpp com comentarios

--- Fila.hpp
+++ Fila.hpp
@@ -8,48 +8,62 @@
 // Estrutura da fila
 template<class Gen>
 class Fila{
-private:
+private: //atributos privados
 	int primeiro;
 	int ultimo;
 	int nElementos;
     Gen elementos[20000];
-public:
+public: //atributos publicos
 	Fila();
 	~Fila();
 	void Insere( Gen &, bool &);
 	void Retira(Gen  &, bool &);
-//	void RetiraAteElemento(Gen & x, bool & deuCerto);
 	bool Vazia() const;
     bool Cheia() const;
     int getNElementos();
     Gen* getDesenhoRastro();
 };
-/*
-	Implementação dos métodos da Fila
-*/
+
+//	Implementação dos métodos da Fila
+
+//construtor
+//Cria uma Fila com os atributos: primeiro, ultimo e numero de Elementos
 template<class Gen>
-Fila<Gen>::Fila(){ // Cria()
+Fila<Gen>::Fila(){
 	ultimo = 0;
 	primeiro = 0;
 	nElementos = 0;
 };
+//fim destrutor
+
+//Vazia()
+//verifica se a Fila está vazia ou não
+//retorna true se estiver, false se não estiver
 template<class Gen>
 bool Fila<Gen>::Vazia() const {
 	if(nElementos == 0)
 		return true;
 	return false;
 };
+//fim Vazia()
 
+//Cheia()
+//verifica se a Fila está cheia ou não
+//retorna true se estiver, false se não estiver
 template<class Gen>
 bool Fila<Gen>::Cheia() const {
 	if(nElementos == 20000)
 		return true;
 	return false;
 };
+//fim Cheia()
 
+//Insere()
+//recebe por referência X do tipo Gen e deuCerto do tipo boolean
+//se a Fila não estiver cheia, entao um elemento é inserido
 template<class Gen>
 void Fila<Gen>::Insere( Gen & x, bool& deuCerto){
-	
+
 	if(Cheia())
         deuCerto=false;
     else{
@@ -58,60 +72,61 @@ void Fila<Gen>::Insere( Gen & x, bool& deuCerto){
         elementos[ultimo]= x;
         if(ultimo==nElementos-1){
             ultimo=0;
-        }else{
+        }
+        else{
             ultimo++;
         }
-        
     }
 };
+//fim Insere
 
-
+//Retira()
+////recebe por referência X do tipo Gen e deuCerto do tipo boolean
+//se a Fila não estiver vazia, entao um elemento é retirado
 template<class Gen>
 void Fila<Gen>::Retira(Gen &x, bool & deuCerto){
 	if(Vazia()){
 		deuCerto = false;
-    }else{
+    }
+    else{
         deuCerto=true;
         nElementos--;
         x=elementos[primeiro];
         if(primeiro==nElementos-1){
             primeiro=0;
-        }else{
+        }
+        else{
             primeiro++;
         }
     }
-        
 };
-	
+//fim Retira()
 
+//Destrutor
+//Retira elemento por elemento da fila, até que fique vazia e seja destruida
 template<class Gen>
-Fila<Gen>::~Fila(){ // Destroi()
+Fila<Gen>::~Fila(){
 	Gen x;
 	bool deuCerto = true;
 	while(!Vazia()){
-// 		Retira(x, deuCerto);
+ 		Retira(x, deuCerto);
 	}
 };
+//fim destrutor
 
-/*
-template<class Gen>
-void Fila<Gen>::RetiraAteElemento(Gen & x, bool & deuCerto){
-	
-	Gen y;
-    do{
-        Retira(y,deuCerto);
-    }while(y.position!=x.position);
-
-};
-*/
-template<class Gen>
-Gen* Fila<Gen>::getDesenhoRastro(){
-    
-    return elementos;
-};
-
+//getNElementos()
+//retorna o numero de elementos da fila
 template<class Gen>
 int Fila<Gen>::getNElementos(){
     return nElementos;
-    
+
 };
+//fim getNElementos()
+
+//getDesenhoRastro()
+//retorna os elementos da rastro da moto
+template<class Gen>
+Gen* Fila<Gen>::getDesenhoRastro(){
+    return elementos;
+};
+//fim getDesenhoRastro()
